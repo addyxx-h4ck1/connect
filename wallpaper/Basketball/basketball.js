a84d3251e9573bb067d953e23a61a070df9e4c6e
addyxx-h4ck1 <briannjosh23@gmail.com>
2023-07-26 05:54:09 +0300

    First Commit

---
+++ wallpaper/Basketball/basketball.js
@@ -0,0 +1,56 @@
+///AI IMAGES ONLY///////////
+
+const getData = fetch ("data.json")
+.then(response =>{
+    return response.json();
+})
+.then(jsonresponse =>{
+    return jsonresponse.basketball;
+})
+.then(imagesPaths =>{
+    imagesPaths.forEach(url =>{
+        const mainsection = document.querySelector(".mainSection");
+
+        const ImageCard = document.createElement('div');
+        ImageCard.setAttribute("class", "maincard");
+        
+
+        const image = document.createElement('img');
+        image.setAttribute(`src`, `${url}`);
+
+        const donloadfunction = document.createElement('a');
+        donloadfunction.setAttribute(`href`, `${url}`);
+        donloadfunction.className = "btn"
+        donloadfunction.setAttribute(`download`, "");
+        donloadfunction.textContent = "Download"
+
+        const downloadIcon = document.createElement('i');
+        downloadIcon.setAttribute("class", "fa fa-download");
+
+        ImageCard.appendChild(image);
+        donloadfunction.appendChild(downloadIcon)
+        ImageCard.appendChild(donloadfunction);
+
+        mainsection.appendChild(ImageCard)
+        
+        /* FULLSCREEN IMAGE */
+         
+        const fullscreenImage = document.createElement('a');
+        fullscreenImage.setAttribute(`href`, `${url}`);
+
+        fullscreenImage.appendChild(image);
+        fullscreenImage.appendChild(donloadfunction)
+        mainsection.appendChild(fullscreenImage)
+
+       
+
+        /*  event */
+
+        donloadfunction.addEventListener("click", (e) =>{
+            donloadfunction.textContent = 'Image Downloaded'
+           donloadfunction.style.backgroundColor = "grey"
+        })
+    })
+
+})
+
